9abc65aaa80257f518fcf140f5e56cd4f2cc0714
BangZ <i@bangz.me>
2018-08-12 20:58:18 +0800

    modify annotation

--- src/sort/selection.js
+++ src/sort/selection.js
@@ -1,8 +1,8 @@
 // 选择排序
 
-
 /**
  * 直接选择排序
+ * 直接选择排序是最直观的也是最容易想出来的排序，但是在性能上存在欠缺
  * 1. 开始时整个线性表为无序表,有序表为空.
  * 2. 将无序表的第一个元素A[0]与其后的每个元素A[i] (i=1,2,3…n)作比较, 若A[0]较大,将A[i]交换.最后得到的第一个元素将是整个线性表中最小的元素.这样有序表元素+1,无序表元素-1;
  * 3. 重复第 2 步,直到无序表长度为0;
@@ -15,7 +15,7 @@
  */
 function straightSelectionSort(arr, order="ASC") {
   var i, j, tmp, tmpidx;
-  for (i = 0; i < arr.length; i++) {
+  for (i = 0; i < arr.length - 1; i++) {    // 外循环只需执行 n-1 次，因为前面排好后，最后一个数必定有序
     tmpidx = i;
     for (j = i+1; j < arr.length; j++) {
       if (order === "ASC" && arr[tmpidx] > arr[j]) {
@@ -25,7 +25,7 @@ function straightSelectionSort(arr, order="ASC") {
         tmpidx = j;
       }
     }
-    if (tmpidx !== i) {
+    if (tmpidx !== i) {   // 对找到的最值与当前位置的值进行交换
       tmp = arr[i];
       arr[i] = arr[tmpidx];
       arr[tmpidx] = tmp;
@@ -33,8 +33,6 @@ function straightSelectionSort(arr, order="ASC") {
   }
   return arr;
 }
-
-
 
 export default {
   straightSelectionSort,
